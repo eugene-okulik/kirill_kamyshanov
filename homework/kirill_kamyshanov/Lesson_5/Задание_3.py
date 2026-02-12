2e2edbb5b859ccbaa2bb29c3f4f5d9796674906d
Кирилл Камышанов <nestor10000@mail.ru>
2026-02-12 19:54:55 +0300

    Homework_5 done

---
+++ homework/kirill_kamyshanov/Lesson_5/Задание_3.py
@@ -0,0 +1,7 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+print(
+    f'Students {students[0]}, {students[1]}, {students[2]} '
+    f'study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}'
+)
