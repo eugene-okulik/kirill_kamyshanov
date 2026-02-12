5593710bb019788fea0fb37139dc30e582e930b5
Кирилл Камышанов <nestor10000@mail.ru>
2026-02-12 12:16:20 +0300

    Homework_3_done

---
+++ homework/kirill_kamyshanov/3.py
@@ -0,0 +1,10 @@
+# Даны два числа. Найти среднее арифметическое и среднее геометрическое этих чисел
+
+x = 9
+y = 3
+arithmetic_av = (x + y) / 2
+geometric_av = (x * y) ** 0.5
+print(arithmetic_av)
+print(geometric_av)
+
+
