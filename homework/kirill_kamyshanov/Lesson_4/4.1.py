a281761221b01521b7f31658cc65ef9dfb166eb6
Кирилл Камышанов <nestor10000@mail.ru>
2026-02-12 16:53:49 +0300

    fixed linter warnings

--- homework/kirill_kamyshanov/Lesson_4/4.1.py
+++ homework/kirill_kamyshanov/Lesson_4/4.1.py
@@ -1,19 +1,19 @@
 my_dict = {
-        'tuple': ('some', 'words', 3, False, 2.456),
-        'list': ['abobo', 4.56, ['willow', 'nose'], (45, 67, 78), {'props': 'yes', 'mater': 'leather'}],
-        'dict': {'name': 'Yan', 'age' : 95, 'occupation': 'plumber', 'hobbies': 'booze', 'is_married': True},
-        'set': {'no pain', 'no gain', 604, False, True}
+    'tuple': ('some', 'words', 3, False, 2.456),
+    'list': ['abobo', 4.56, ['willow', 'nose'], (45, 67, 78), {'props': 'yes', 'mater': 'leather'}],
+    'dict': {'name': 'Yan', 'age': 95, 'occupation': 'plumber', 'hobbies': 'booze', 'is_married': True},
+    'set': {'no pain', 'no gain', 604, False, True}
 }
 
-print(my_dict['tuple'][-1]) # выведите на экран последний элемент
+print(my_dict['tuple'][-1])  # выведите на экран последний элемент
 
-my_dict['list'].append('one_more]') # добавьте в конец списка еще один элемент
-del my_dict['list'][1] # удалите второй элемент списка
+my_dict['list'].append('one_more]')  # добавьте в конец списка еще один элемент
+del my_dict['list'][1]  # удалите второй элемент списка
 
-my_dict['dict'][('i am a tuple',)] = 'really' # добавьте элемент с ключом ('i am a tuple',) и любым значением
-del my_dict['dict']['hobbies'] # удалите какой-нибудь элемент
+my_dict['dict'][('i am a tuple',)] = 'really'  # добавьте элемент с ключом ('i am a tuple',) и любым значением
+del my_dict['dict']['hobbies']  # удалите какой-нибудь элемент
 
-my_dict['set'].add(7) # добавьте новый элемент в множество
-my_dict['set'].remove(False) # удалите элемент из множества
+my_dict['set'].add(7)  # добавьте новый элемент в множество
+my_dict['set'].remove(False)  # удалите элемент из множества
 
-print(my_dict) # В конце выведите на экран весь словарь
+print(my_dict)  # В конце выведите на экран весь словарь
