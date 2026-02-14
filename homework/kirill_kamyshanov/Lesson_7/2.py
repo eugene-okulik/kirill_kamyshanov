159019d90a05638c0a7f3d5d46ad520ce2cd939c
Кирилл Камышанов <nestor10000@mail.ru>
2026-02-14 09:54:54 +0300

    fixed linter notifications

--- homework/kirill_kamyshanov/Lesson_7/2.py
+++ homework/kirill_kamyshanov/Lesson_7/2.py
@@ -1,7 +1,9 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
+
 def multiple_rep(data):
     for key, value in words.items():
         print(key * value)
+
 
 multiple_rep(words)
--- homework/kirill_kamyshanov/Lesson_7/3.py
+++ homework/kirill_kamyshanov/Lesson_7/3.py
@@ -3,9 +3,11 @@ example2 = 'результат операции: 54'
 example3 = 'результат работы программы: 209'
 example4 = 'результат: 2'
 
+
 def parse_and_increase(data):
     result = int(data.split()[-1]) + 10
     print(result)
+
 
 parse_and_increase(example1)
 parse_and_increase(example2)
