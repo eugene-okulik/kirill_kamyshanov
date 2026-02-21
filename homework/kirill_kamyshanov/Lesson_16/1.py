8f921c9172a1d481a554bc92bc6cac51f84e7b6b
Кирилл Камышанов <nestor10000@mail.ru>
2026-02-21 11:32:45 +0300

    homework_16 done

--- homework/kirill_kamyshanov/Lesson_16/1.py
+++ homework/kirill_kamyshanov/Lesson_16/1.py
@@ -1,29 +1,64 @@
 import csv
 import os
-# модуль для БД и дотенв (модуль скачан)
+import mysql.connector as mysql
+import dotenv
+
+dotenv.load_dotenv()
 
 base_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-full_path = os.path.join(base_path,'eugene_okulik', 'Lesson_16', 'hw_data', 'data.csv')
+full_path = os.path.join(base_path, 'eugene_okulik', 'Lesson_16', 'hw_data', 'data.csv')
 
 with open(full_path, 'r', encoding='utf8', newline='') as csv_file:
     reader = csv.reader(csv_file)
-    for row in reader:
+    file_data = list(reader)
+
+db = mysql.connect(
+    user=os.getenv('DB_USER'),
+    passwd=os.getenv('DB_PASSW'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT'),
+    database=os.getenv('DB_NAME')
+)
+
+cursor = db.cursor()
+
+query = """
+SELECT s.name,
+s.second_name,
+g.title,
+b.title,
+subj.title,
+l.title,
+m.value
+FROM `groups` g
+JOIN students s ON s.group_id = g.id
+JOIN books b ON  b.taken_by_student_id = s.id
+JOIN marks m ON m.student_id = s.id
+JOIN lessons l ON m.lesson_id = l.id
+JOIN subjects subj ON subj.id = l.subject_id
+"""
+
+
+def read_db():
+    cursor.execute(query)
+    response = cursor.fetchall()
+    for line in response:
+        yield line
+
+
+file_set = set(tuple(row) for row in file_data)
+found_in_db = set()
+
+for db_row in read_db():
+    db_row = tuple(db_row)
+    if db_row in file_set:
+        found_in_db.add(db_row)
+
+missing_data = file_set - found_in_db
+
+if missing_data:
+    print('Данные, отсутствующие в БД:')
+    for row in missing_data:
         print(row)
-
-
-
-# Прочитайте этот файл с помощью модуля csv и проверьте есть ли те данные,
-# которые там перечислены, в нашей базе данных.
-#
-# При подключении к базе данных не прописывайте данные подключения в коде,
-# а воспользуйтесь подходом .env c такими переменными:
-# DB_USER, DB_PASSW, DB_HOST, DB_PORT, DB_NAME.
-# Я на своем компе уже создал файл .env с этими переменными,
-# так что, если все сделаем одинаковые названия, будет работать всё универсально
-#
-# В результате сравнения, если обнаружится, что каких-то данных,
-# которые есть в файле, нет в базе данных, распечатайте каких данных не хватает в базе.
-#
-# Подсказка для самопроверки: в базе нет данных,
-# которые полностью соответствуют первой строке файла,
-# но в базе есть данные, которые соответствуют второй строке файла.
+else:
+    print('Все записи из файла есть в БД')
