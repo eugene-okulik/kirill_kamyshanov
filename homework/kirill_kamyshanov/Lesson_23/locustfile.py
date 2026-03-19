aa6bfc664792772c7c08b35e7d25c6a5ecc9d438
kirill_kamyshanov <nestor10000@mail.ru>
2026-03-19 19:48:51 +0300

    homework_23 done

--- homework/kirill_kamyshanov/Lesson_23/locustfile.py
+++ homework/kirill_kamyshanov/Lesson_23/locustfile.py
@@ -1,108 +1,62 @@
-from locust import task, HttpUser
+from locust import task, HttpUser, between
+import random
 
-# 'http://objapi.course.qa-practice.com/object
 
+# хост: http://objapi.course.qa-practice.com/object
+
+# Я написал два класса (два разных способа. Первый способ закомментил - берём id из числа существующих
+# class ManipulationsObjects(HttpUser):
+#     wait_time = between(1, 3)
+#     new_body = {"name": "stranger", "data": {"feature": "value"}}
+#     put_body = {"name": "Wasya", "data": {"is_smart": "a_lot"}}
+#     body = {"data": {"feature": "value"}}
+#
+#     existing_ids = [5056, 5057, 5058, 5059, 5060, 5061, 5062]
+#
+#     @task(4)
+#     def get_one_obj(self):
+#         self.client.get(f'/{random.choice(self.existing_ids)}')
+#
+#     @task(1)
+#     def get_all_obj(self):
+#         self.client.get('')
+#
+#     @task(1)
+#     def put_edit_obj(self):
+#         self.client.put(f'/{random.choice(self.existing_ids)}', json=self.put_body)
+#
+#     @task(2)
+#     def patch_edit_obj(self):
+#         self.client.patch(f'/{random.choice(self.existing_ids)}', json=self.body)
+
+
+# Второй способ (ниже) мне нравится больше. Во-первых, здесь мы не зависим от того, существуют ли передаваемые id в БД
+# или уже удалены. И во-вторых, здесь мы дополнительно проверим работу POST и DELETE методов.
 class ManipulationsObjects(HttpUser):
+    wait_time = between(1, 3)
+    new_body = {"name": "stranger", "data": {"feature": "value"}}
+    put_body = {"name": "Wasya", "data": {"is_smart": "a_lot"}}
+    body = {"data": {"feature": "value"}}
+
+    def on_start(self):
+        self.response = self.client.post('', json=self.new_body)
+        self.obj_id = self.response.json().get('id')
+
+    @task(4)
     def get_one_obj(self):
-        request = self.client.get(f'/{id}')
+        self.client.get(f'/{self.obj_id}')
 
     @task(1)
     def get_all_obj(self):
-        request = self.client.get('')
+        self.client.get('')
 
-
-    def create_an_obj(self):
-        request = self.client.post('/', json={"name": "stranger","data": {"feature": "value"}})
-
-
+    @task(1)
     def put_edit_obj(self):
-        request = self.client.put(f'/{1}', json={"name": "stranger","data": {"feature": "value"}})
+        self.client.put(f'/{self.obj_id}', json=self.put_body)
 
+    @task(2)
     def patch_edit_obj(self):
-        request = self.client.patch(f'/{1}', json={"name": "stranger","data": {"feature": "value"}})
+        self.client.patch(f'/{self.obj_id}', json=self.body)
 
-
-    def delete_an_obj(self):
-        request = self.client.delete(f'/{1}')
-
-
-# import requests
-
-
-# # Служебная функция для удаления тестовых объектов
-# def x_clear(object_id):
-#     requests.delete(f'http://objapi.course.qa-practice.com/object/{object_id}')
-#
-#
-# # Служебная функция для создания тестовых объектов
-# def x_new_object():
-#     body = {"name": "Andrey",
-#             "data": {"feature": "value"}
-#             }
-#     response = requests.post('http://objapi.course.qa-practice.com/object', json=body)
-#     return response.json()['id']
-#
-#
-# # Создание объекта
-# def create_obj():
-#     body = {"name": "stranger","data": {"feature": "value"}}
-#     response = requests.post('http://objapi.course.qa-practice.com/object', json=body)
-#     object_id = response.json()['id']
-#     response_body = response.json()  # {'data': {'feature': 'value'}, 'id': 4036, 'name': 'stranger'}
-#     print(response_body)
-#     assert response.status_code == 200, 'Status code is not 200'
-#     assert 'id' in response_body, 'Response body does not have an id'
-#     assert response_body['name'] == 'stranger', 'Name does not match'
-#     assert response_body['data'] == {"feature": "value"}, 'Feature does not match'
-#     x_clear(object_id)
-#
-#
-# # Изменение объекта с помощью метода PUT
-# def put_edit_obj():
-#     object_id = x_new_object()
-#     body = {"name": "new_name_put",
-#             "data": {"feature": "new_value_put"}
-#             }
-#     response = requests.put(f'http://objapi.course.qa-practice.com/object/{object_id}', json=body)
-#     # Евгений, привет. Здесь мне показалось странным, что поле 'id' приходит в строковом формате, а не в числовом.
-#     # В методе x_new_object это поле возвращается как число. Баг для проверки внимательности или особенность?
-#     response_body = response.json()  # {'data': {'feature': 'new_value_put'}, 'id': '4041', 'name': 'new_name_put'}
-#     print(response_body)
-#     assert response.status_code == 200, 'Status code is not 200'
-#     assert int(response_body['id']) == object_id, 'ID does not match'
-#     assert response_body['name'] == 'new_name_put', 'Name does not match'
-#     assert response_body['data'] == {"feature": "new_value_put"}, 'Feature does not match'
-#     x_clear(object_id)
-#
-#
-# # Изменение объекта с помощью метода PATCH
-# def patch_edit_obj():
-#     object_id = x_new_object()
-#     body1 = {"name": "new_name_patch"}
-#     body2 = {"data": {"feature": "new_value_patch"}}
-#     response = requests.patch(f'http://objapi.course.qa-practice.com/object/{object_id}', json=body1)
-#     response_body = response.json()
-#
-#     assert response.status_code == 200, 'Status code is not 200'
-#     assert response_body['name'] == 'new_name_patch', 'Name does not match'
-#     assert 'id' in response_body, 'Response body does not have an id'
-#     assert 'data' in response_body, 'Response body does not have a data'
-#
-#     response = requests.patch(f'http://objapi.course.qa-practice.com/object/{object_id}', json=body2)
-#     response_body = response.json()
-#     assert response.status_code == 200, 'Status code is not 200'
-#     assert response_body['name'] == 'new_name_patch', 'Name does not match'
-#     assert response_body['data'] == {"feature": "new_value_patch"}, 'Response body does not have a data'
-#     assert 'id' in response_body, 'Response body does not have an id'
-#     x_clear(object_id)
-#
-#
-# # Удаление объекта
-# def delete_an_obj():
-#     object_id = x_new_object()
-#     response = requests.delete(f'http://objapi.course.qa-practice.com/object/{object_id}')
-#     assert response.status_code == 200, 'Status code is not 200'
-#
-#     # Проверка того, что объект удалился
-#     query = requests.get(f'http://objapi.course.qa-practice.com/object/{object_id}')
-#     assert query.status_code == 404, 'Status code is not 404'
+    def on_stop(self):
+        self.client.delete(f'/{self.obj_id}')
